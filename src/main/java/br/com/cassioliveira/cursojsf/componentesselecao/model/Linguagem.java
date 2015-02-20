65ecbe1b7c1925fdef1f5d6d52272db47e21ce83
Cássio Oliveira <cassio@cassioliveira.com.br>
2015-02-19 23:44:50 -0300

    Adicionado link para voltar para página de seleção e comentários.

--- src/main/java/br/com/cassioliveira/cursojsf/componentesselecao/model/Linguagem.java
+++ src/main/java/br/com/cassioliveira/cursojsf/componentesselecao/model/Linguagem.java
@@ -1,11 +1,13 @@
 package br.com.cassioliveira.cursojsf.componentesselecao.model;
 
+import javax.persistence.Entity;
+
 /**
  *
  * @author cassio
  */
 public class Linguagem {
-
+    
     private String linguagemFavorita;
     
     private boolean melhorLinguagem;
